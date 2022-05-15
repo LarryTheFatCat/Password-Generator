d9453054169038f5e178a3bf69ba5472b66b89ed
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-05-15 00:22:11 -0400

    added copy to clipboard via string builder

--- src/main/java/main/RandomStringExample.java
+++ src/main/java/main/RandomStringExample.java
@@ -1,17 +1,13 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
-/**
- * @author TwoOneTwo :)
- * Two#6969 <- if you have any questions
- * This is just an example of Char & String Generator.
- */
-
-// TODO: Figure out how to make it so if you close it shows a warning asking if you want to copy it to clipboard postClose.
 public class RandomStringExample extends JOptionPane {
     /**
      * Gets alphanumeric string.
@@ -20,16 +16,20 @@ public class RandomStringExample extends JOptionPane {
      */
     public static String getAlphaNumericString(int n) {
 
-        JFrame frame = new JFrame("Password");
+        JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // length is bounded by 256 Character
         byte[] array = new byte[256];
         new Random().nextBytes(array);
-
         String randomString = new String(array, StandardCharsets.UTF_8);
+
 
         // Create a StringBuffer to store the result
         StringBuilder r = new StringBuilder();
+
+        // @Credit Lazzzy <3
+        sendToClipBoard(r);
+
 
         /* Append first 20 alphanumeric characters
         from the generated random String into the result
@@ -47,10 +47,13 @@ public class RandomStringExample extends JOptionPane {
                 n--;
             }
         }
+
+        // @Credit Lazzzy <3
+        sendToClipBoard(r);
         // PrintWriter && JOptionPane both use Try catch... Why not put both in 1 :)), fixed all bugs as well
         try (PrintWriter out = new PrintWriter("output-info.txt")) {
             out.println(r);
-            JOptionPane.showConfirmDialog(frame, r + "\n" + "Generated New Text File Storing information!" + "\n" + "Do you want it copied to clipboard?");
+            JOptionPane.showMessageDialog(frame, r + "\n" + "Generated New Text File Storing information & copied to clipboard!");
         } catch (Exception e) {
             throw new RuntimeException("Interrupted during process...");
         }
@@ -58,10 +61,13 @@ public class RandomStringExample extends JOptionPane {
 
         // return the resultant string
         return r.toString();
-
-
     }
 
+    private static void sendToClipBoard(StringBuilder stringBuilder){
+        StringSelection stringSelection = new StringSelection(stringBuilder.toString());
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+    }
 
 
     public static void main(String[] args) {
