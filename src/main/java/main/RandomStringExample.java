607dc9db697b262415b77628ddf4c84a0fb13dde
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-05-18 02:42:52 -0400

    meow

--- src/main/java/main/RandomStringExample.java
+++ src/main/java/main/RandomStringExample.java
@@ -2,11 +2,13 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.datatransfer.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
+@SuppressWarnings("PARAMETER")
 public class RandomStringExample extends JOptionPane {
     /**
      * Gets alphanumeric string.
@@ -26,7 +28,8 @@ public class RandomStringExample extends JOptionPane {
         // Create a StringBuffer to store the result
         StringBuilder r = new StringBuilder();
 
-        /* Append first 20 alphanumeric characters
+        /*
+        Append first 20 alphanumeric characters
         from the generated random String into the result
          */
         for (int k = 0; k < randomString.length(); k++) {
@@ -42,11 +45,9 @@ public class RandomStringExample extends JOptionPane {
                 n--;
             }
         }
-
-        // @Credit Lazy <3
         sendToClipBoard(r);
 
-        // PrintWriter && JOptionPane both use Try catch... Why not put both in 1 :)), fixed all bugs as well
+
         try (PrintWriter out = new PrintWriter("output-info.txt")) {
 
             out.println(r);
@@ -54,8 +55,6 @@ public class RandomStringExample extends JOptionPane {
         } catch (Exception e) {
             throw new RuntimeException("Interrupted during process...");
         }
-
-
         // return the resultant string
         return r.toString();
     }
@@ -65,7 +64,6 @@ public class RandomStringExample extends JOptionPane {
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(stringSelection, null);
     }
-
 
     public static void main(String[] args) {
         // size of random alphanumeric string
