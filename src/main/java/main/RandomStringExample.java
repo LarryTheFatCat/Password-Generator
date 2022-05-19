df3fda7c166070c63b0a9f7480c7cfc6ab49a909
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-05-19 18:12:08 -0400

    Reduced code spacing & lot easier to read (I hope)

--- src/main/java/main/RandomStringExample.java
+++ src/main/java/main/RandomStringExample.java
@@ -9,28 +9,16 @@ import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
 public class RandomStringExample extends JOptionPane {
-    /**
-     * Gets alphanumeric string.
-     *
-     * @return the alphanumeric string
-     */
     public static String getAlphaNumericString(int n) {
-
-        JFrame frame = new JFrame("meow");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // length is bounded by 256 Character
         byte[] array = new byte[256];
         new Random().nextBytes(array);
         String randomString = new String(array, StandardCharsets.UTF_8);
 
-
         // Create a StringBuffer to store the result
         StringBuilder r = new StringBuilder();
 
-        /*
-        Append first 20 alphanumeric characters
-        from the generated random String into the result
-         */
+        // Append first 20 alphanumeric characters from the generated random String into the result
         for (int k = 0; k < randomString.length(); k++) {
 
             char ch = randomString.charAt(k);
@@ -44,28 +32,27 @@ public class RandomStringExample extends JOptionPane {
                 n--;
             }
         }
-        // check line 62 for code
+
         sendToClipBoard(r);
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JOptionPane.showMessageDialog(f, r + "\n" + "Generated New Text File Storing information & copied to clipboard!");
 
-        // We can use a dialog to show the output instead of using an ide, much simpler lol...
-        JOptionPane.showMessageDialog(frame, r + "\n" + "Generated New Text File Storing information & copied to clipboard!");
-
-        try (PrintWriter out = new PrintWriter("output-info.txt")) {
-            out.println(r);
-        } catch (Exception e) {
-            e.printStackTrace();
+        try (PrintWriter o = new PrintWriter("output-info.txt")) {
+            o.println(r);
+        } catch (Exception er) {
+            er.printStackTrace();
         }
+
         // return the resultant string
         return r.toString();
 
     }
-
     private static void sendToClipBoard(StringBuilder stringBuilder){
         StringSelection stringSelection = new StringSelection(stringBuilder.toString());
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(stringSelection, null);
     }
-
     public static void main(String[] args) {
         // size of random alphanumeric string
         int n = 156;
