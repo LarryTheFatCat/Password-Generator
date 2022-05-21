3005dbe466f11c3921b61a1f35dfd06bd2f48c88
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-05-20 23:50:14 -0400

    spaces in text to not look like ramen noodles

--- src/main/java/main/RandomStringExample.java
+++ src/main/java/main/RandomStringExample.java
@@ -14,14 +14,18 @@ public class RandomStringExample extends JOptionPane {
         byte[] array = new byte[256];
         new Random().nextBytes(array);
 
+
         // Create a StringBuffer to store the result
         String randomString = new String(array, StandardCharsets.UTF_8);
         StringBuilder r = new StringBuilder();
 
+
+
         // Append first 20 alphanumeric characters from the generated random String into the result
         for (int k = 0; k < randomString.length(); k++) {
-
             char ch = randomString.charAt(k);
+
+
 
             if (((ch >= 'a' && ch <= 'z')
                     || (ch >= 'A' && ch <= 'Z')
@@ -35,12 +39,22 @@ public class RandomStringExample extends JOptionPane {
         }
         sendToClipBoard(r);
 
+
         // return the resultant string in a JOptionPane.
         JFrame f = new JFrame();
         JOptionPane.showMessageDialog(f, r + "\n" + "Copied to clipboard & creating file...", "password-output", JOptionPane.INFORMATION_MESSAGE);
+
+        // Window closing event listener to close the window opening another JOptionPane.
+        f.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                JFrame f = new JFrame();
+                JOptionPane.showMessageDialog(f, "File created successfully", "password-output", JOptionPane.INFORMATION_MESSAGE);
+                f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            }
+        });
+
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
         // Create a file to write the output.
         try (PrintWriter o = new PrintWriter("output-info.txt", "UTF-8")) {
             o.println(r);
@@ -48,9 +62,9 @@ public class RandomStringExample extends JOptionPane {
             throw new RuntimeException(er);
         }
 
-        // Returns the result
 
     } // end of method
+
 
     // Stores the output in your clipboard...
     private static void sendToClipBoard(StringBuilder stringBuilder) {
@@ -58,6 +72,7 @@ public class RandomStringExample extends JOptionPane {
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(stringSelection, null);
     }
+
 
     public static void main(String[] args) {
         // size of random alphanumeric string
