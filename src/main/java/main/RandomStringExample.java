332e34b97e4ee125363b31abef89c845a9ac6325
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-05-25 00:04:41 -0400

    fix the stupid error when r1 if bracket is set to the one that is at the last one meaning if that ends then the whole program will crash. Include thank you screen at the very end.

--- src/main/java/main/RandomStringExample.java
+++ src/main/java/main/RandomStringExample.java
@@ -10,9 +10,15 @@ import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
 public class RandomStringExample {
+    
+    public static final String main_name = "PasswordGenerator";
+    public static final String version = "Version 1.0";
+    public static final String author = "TwoOneTwo";
+    public static final String github_account = "https://github.com/TwoThreeTwo";
+    public static final String github_repo = "https://github.com/TwoThreeTwo/RandomStringGenerator";
+
 
     public static void getAlphaNumericString(int n) {
-
         // length is bounded by 256 Chars as per the Ascii Table (2^8).
         byte[] array = new byte[256];
         new Random().nextBytes(array);
@@ -43,7 +49,7 @@ public class RandomStringExample {
         }
         int r7 = JOptionPane.showConfirmDialog(null, "Would you like to generate a password?", "Password Generator", JOptionPane.YES_NO_OPTION);
         if (r7 == JOptionPane.YES_OPTION) {
-            JOptionPane.showMessageDialog(null, "Your new password is " + r, "Password Generator", JOptionPane.QUESTION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Your new password is " + r, main_name, JOptionPane.QUESTION_MESSAGE);
         } else {
             JOptionPane.showMessageDialog(null, "Sorry to hear you won't be creating one...");
         }
@@ -61,81 +67,17 @@ public class RandomStringExample {
         }
 
 
-        // Fix this shit over here bro I'm bout to kill my self
+        // Fix this shit over here bro I'm about to kill my self
 
 
 
 
-
-
-
-
-
-
-
-
-
-        // Ask if you want to see the amount of chars inside the file
-        int r3 = JOptionPane.showConfirmDialog(null, "Would you like to see the amount of characters in the file before you create a file?", "Password Generator", JOptionPane.YES_NO_OPTION);
-        if (r3 == JOptionPane.YES_OPTION) {
-            JOptionPane.showMessageDialog(null, "There are a total of " + r.length() + " characters in this string.", "Password Generator", JOptionPane.QUESTION_MESSAGE);
-        } else {
-            JOptionPane.showMessageDialog(null, "Canceling...", "Password Generator", JOptionPane.QUESTION_MESSAGE);
-            JOptionPane.showMessageDialog(null, "Cancelled!", "Password Generator", JOptionPane.QUESTION_MESSAGE);
-        }
-    }
-    // end of method getAlphaNumericString.
-
-    // Method to copy the generated String to the clipboard.
-    private static void sendToClipBoard(StringBuilder stringBuilder) {
-        StringSelection stringSelection = new StringSelection(stringBuilder.toString());
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        clipboard.setContents(stringSelection, null);
-    }
-
-    // Main method
-    public static void main(String[] args) {
-        getAlphaNumericString(20);
-        // Thread sleeps after 142 nanoseconds it wakes up and prints the generated String.
-        try {
-            Thread.sleep(142);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-    }
-}
-/*
-
-
-        String fileExtension = JOptionPane.showInputDialog("Enter file extension.");
-        if(fileExtension.equals("")){
-            JOptionPane.showMessageDialog(null, "No file extension entered. Defaulting to .txt");
-            fileExtension = ".txt";
-            try{
-                PrintWriter writer = new PrintWriter("randomString" + fileExtension, "UTF-8");
-                writer.println(r);
-                writer.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            try{
-                PrintWriter writer = new PrintWriter("randomString" + fileExtension, "UTF-8");
-                writer.println(r);
-                writer.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
- */
-
-/*
         int r1 = JOptionPane.showConfirmDialog(null, "Would you like it as a file?", "Password Generator", JOptionPane.YES_NO_OPTION);
         if (r1 == JOptionPane.YES_OPTION) {
             int r8 = JOptionPane.showConfirmDialog(null, "Would you like a custom file extension?", "Password Generator", JOptionPane.YES_NO_OPTION);
+            String fileExtension = null;
             if (r8 == JOptionPane.YES_OPTION) {
-                String fileExtension = JOptionPane.showInputDialog("Enter file extension.");
+                fileExtension = JOptionPane.showInputDialog("Enter file extension.");
                 if (fileExtension.equals("")) {
                     JOptionPane.showMessageDialog(null, "No file extension entered. Defaulting to .txt");
                     fileExtension = ".txt";
@@ -148,7 +90,13 @@ public class RandomStringExample {
                     }
 
                 } else {
-                    JOptionPane.showMessageDialog(null, "Not generating file...", "Password Generator", JOptionPane.ERROR_MESSAGE);
+                    try {
+                        PrintWriter writer = new PrintWriter("randomString." + fileExtension, "UTF-8");
+                        writer.println(r);
+                        writer.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             } else {
                 if (!new File("Password Generator-info." + fileExtension).exists()) {
@@ -184,16 +132,43 @@ public class RandomStringExample {
                 }
 
 
-            } else{
-                JOptionPane.showMessageDialog(null, "Canceling save...", "Password Generator", JOptionPane.QUESTION_MESSAGE);
-                JOptionPane.showMessageDialog(null, "Canceled!", "Password Generator", JOptionPane.QUESTION_MESSAGE);
-
-                System.exit(0);
             }
+        } else{
+            JOptionPane.showMessageDialog(null, "Canceling save...", "Password Generator", JOptionPane.QUESTION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Canceled!", "Password Generator", JOptionPane.QUESTION_MESSAGE);
+
+            System.exit(0);
         }
+        // Ask if you want to see the amount of chars inside the file
+        int r3 = JOptionPane.showConfirmDialog(null, "Would you like to see the amount of characters in the file before you create a file?", "Password Generator", JOptionPane.YES_NO_OPTION);
+        if (r3 == JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, "There are a total of " + r.length() + " characters in this string.", "Password Generator", JOptionPane.QUESTION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "Canceling...", "Password Generator", JOptionPane.QUESTION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Cancelled!", "Password Generator", JOptionPane.QUESTION_MESSAGE);
+        }
+        JOptionPane.showMessageDialog(null,"Thank you for using " + main_name + "\nThe person who created this program is " + author + "\nThe version this program is currently on is " + version + "\n If you have the JAR file but not the repository, you can check it out here " + github_repo + "\n The GitHub account that currently made it is " + github_account , "Password Generator", JOptionPane.QUESTION_MESSAGE);
+
     }
- */
 
+    // end of method getAlphaNumericString.
 
+    // Method to copy the generated String to the clipboard.
+    private static void sendToClipBoard(StringBuilder stringBuilder) {
+        StringSelection stringSelection = new StringSelection(stringBuilder.toString());
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+    }
 
+    // Main method
+    public static void main(String[] args) {
+        getAlphaNumericString(20);
+        // Thread sleeps after 142 nanoseconds it wakes up and prints the generated String.
+        try {
+            Thread.sleep(142);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+    }
+}
