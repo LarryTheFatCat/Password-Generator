07b95ea4e85e8fb7113ad86f0ff546fb614a0dd7
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-06-10 21:24:41 -0400

    update todo

--- src/main/java/values/Variables.java
+++ src/main/java/values/Variables.java
@@ -2,7 +2,7 @@ package values;
 
 public class Variables {
     public static final String mn = "PasswordGenerator";
-    public static final String v = "v 1.0";
+    public static final String v = "v-1.0";
     public static final String a = "TwoOneTwo";
     public static final String ghAcc = "https://github.com/TwoThreeTwo";
     public static final String ghRepo = "https://github.com/TwoThreeTwo/RandomStringGenerator";
